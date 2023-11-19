51d7ad8c77b3d81cc7cc7bdc22d97ea71a20e76d
Daniela Cruz Mejía <123124430+dantrixxheron@users.noreply.github.com>
2023-11-19 02:26:58 +0000

    Refactor sidebar component and update styles

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,22 +1,30 @@
 // Sidebar.tsx
 import React, { useState } from 'react';
-import '../index.css';
 import SidebarContent from './sidebarContent';
+import './sidebar.css';
+import { IoArrowBack, IoArrowForward } from "react-icons/io5";
+import textbox from '../components/textbox'
+
 const Sidebar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(true);
-
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
   };
 
   return (
-    <div className={`sidebar ${isOpen ? 'open' : 'closed'}`}>
+    <div>
       <button className="toggle-btn" onClick={toggleSidebar}>
-        {isOpen ? '<--' : '-->'}
+        {isOpen ? <IoArrowBack />: <IoArrowForward/>}
       </button>
-      {/* Agrega aquí el contenido de tu barra lateral */}
-      <SidebarContent icon='db' />
-      <SidebarContent icon='settings'/>
+      <div className={`sidebar ${isOpen ? 'open' : 'closed'}`}>
+        {/* Agrega aquí el contenido de tu barra lateral */}
+        <div className="row-setings">
+          <SidebarContent icon='home' onclick={()=>window.location.href='#'}/>
+          <SidebarContent icon='settings' onclick={()=>window.location.href='https://github.com'}/>
+          <SidebarContent icon='reload' onclick={()=>"#"}/>
+        </div>
+        <SidebarContent icon='db' info="Database name" onclick={()=>"#"} />
+      </div>
     </div>
   );
 };
--- src/components/sidebarContent.tsx
+++ src/components/sidebarContent.tsx
@@ -1,40 +1,53 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { BiSolidCoinStack } from "react-icons/bi";
-import { IoMdSettings } from "react-icons/io";
-interface SidebarContentProps {
-    icon: string;
-}
-const SidebarContent: React.FC<SidebarContentProps> = ({ icon }) => {
-    const [text, setText] = useState("");
-    const IconComponent = ()=>{
-        if(icon == 'db'){
-            return <BiSolidCoinStack />
-        }else if(icon == 'settings'){
-            return <IoMdSettings />
-        }
-    }
-    // useEffect(() => {
-    //     // Realizar solicitud a la base de datos para obtener el nombre de la base de datos en la raíz
-    //     // Aquí debes implementar la lógica para obtener el nombre de la base de datos desde la BD
-    //     const obtenerNombreBaseDeDatos = async () => {
-    //         try {
-    //             const response = await fetch("URL_DE_LA_API_PARA_OBTENER_NOMBRE_BD");
-    //             const data = await response.json();
-    //             const nombreBD = data.nombre; // Suponiendo que el nombre de la base de datos se encuentra en la propiedad "nombre" del objeto de respuesta
-    //             setText(nombreBD);
-    //         } catch (error) {
-    //             console.error("Error al obtener el nombre de la base de datos:", error);
-    //         }
-    //     };
+import { IoSettings, IoReloadCircle, IoHomeSharp } from "react-icons/io5";
 
-    //     obtenerNombreBaseDeDatos();
-    // }, []);
-    return (
-        <div className="text-sidebar">            
-            <p> {IconComponent()} Database Name</p>
-            {/* <p> {IconComponent} {text}</p> */}
-        </div>
-    );
+interface SidebarContentProps {
+  icon: string;
+  info?: string;
+  onclick?: () => void; // Haz la propiedad opcional con ?
+}
+
+const SidebarContent: React.FC<SidebarContentProps> = ({ icon, info, onclick }) => {
+    const [text, setText] = useState("");  
+
+  const IconComponent = () => {
+    if (icon === "settings") {
+      return <IoSettings />;
+    } else if (icon === "reload") {
+      return <IoReloadCircle />;
+    } else if (icon === "home") {
+      return <IoHomeSharp />;
+    } else {
+      return <BiSolidCoinStack />;
+    }
+  };
+
+  // useEffect(() => {
+  //     Realizar solicitud a la base de datos para obtener el nombre de la base de datos en la raíz
+  //     Aquí debes implementar la lógica para obtener el nombre de la base de datos desde la BD
+  //     const obtenerNombreBaseDeDatos = async () => {
+  //         try {
+  //             const response = await fetch("URL_DE_LA_API_PARA_OBTENER_NOMBRE_BD");
+  //             const data = await response.json();
+  //             const nombreBD = data.nombre; // Suponiendo que el nombre de la base de datos se encuentra en la propiedad "nombre" del objeto de respuesta
+  //             setText(nombreBD);
+  //         } catch (error) {
+  //             console.error("Error al obtener el nombre de la base de datos:", error);
+  //         }
+  //     };
+
+  //     obtenerNombreBaseDeDatos();
+  // }, []);
+
+  return (
+    <div className="text-sidebar">
+      <p onClick={onclick}>
+        <IconComponent /> {typeof info === "string" && <span>{info}</span>}
+        {/* {IconComponent()} {text} */}
+      </p>
+    </div>
+  );
 };
 
 export default SidebarContent;
