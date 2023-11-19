efe3a9575ee1f6af4f45089b3d37dd6f78bb4db4
Daniela Cruz Mejía <123124430+dantrixxheron@users.noreply.github.com>
2023-11-19 02:33:34 +0000

    Update sidebar width and font size

--- src/components/textbox.tsx
+++ src/components/textbox.tsx
@@ -5,9 +5,7 @@ const Textbox: React.FC = () => {
   const [query, setQuery] = useState('');
 
   const handleExecuteQuery = () => {
-    // Aquí puedes agregar la lógica para enviar la consulta a la API
-    // Sin embargo, como mencionaste que tus compañeros de trabajo de backend se encargarán de esto,
-    // simplemente dejaremos esta función vacía por ahora.
+    
   };
 
   const handleQueryChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
