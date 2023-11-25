ba21ec6a99ded3c88cc15479b0c40da8d74640cb
Nataly Iglesias <117701434+Nat-Ig07@users.noreply.github.com>
2023-11-25 19:26:57 +0000

    Menu operaciones CRUD

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -8,6 +8,7 @@ interface TableProps {
 
 const Table: React.FC<TableProps> = ({ data, isOpen }) => {
   const [currentPage, setCurrentPage] = useState(1);
+  const [showMenu, setShowMenu] = useState(false);
   const itemsPerPage = 10;
 
   if (data.length === 0) {
@@ -34,32 +35,48 @@ const Table: React.FC<TableProps> = ({ data, isOpen }) => {
     setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
   };
 
+  // Toggle para mostrar/ocultar el menú
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   // Renderizar la tabla
   return (
     <div>
-    <table className={`table ${(isOpen)? 'open': 'closed'}`}>
-      <thead>
-        <tr>
-          {headers.map((header) => (
-            <th key={header}>{header}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-  {currentItems.map((row, index) => (
-    <tr key={index}>
-      {headers.map((header) => (
-        <td key={header}>{row[header]}</td>
-      ))}
-    </tr>
-  ))}
-</tbody>
-    </table>
+      {/* Botón para mostrar/ocultar el menú */}
+      <button className={`btn-toggle-menu ${isOpen ? 'open' : 'closed'}`} onClick={toggleMenu}>:</button>
 
-    <div>
+      {/* Menú de operaciones CRUD */}
+      {showMenu && (
+    <div className={`mini-menu ${isOpen ? 'open' : 'closed'}`}>
+    <button className="btn-agregar">Agregar</button>
+          <button className="btn-modificar">Modificar</button>
+          <button className="btn-eliminar">Eliminar</button>
+        </div>
+      )}
+    
+      <table className={`table ${(isOpen) ? 'open' : 'closed'}`}>
+        <thead>
+          <tr>
+            {headers.map((header) => (
+              <th key={header}>{header}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {currentItems.map((row, index) => (
+            <tr key={index}>
+              {headers.map((header) => (
+                <td key={header}>{row[header]}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <div>
         {/* Botón para ir a la página anterior */}
         <button className={`btnAnterior ${(isOpen) ? 'open' : 'closed'}`} onClick={prevPage} disabled={currentPage === 1}> Anterior </button>
-
         {/* Botón para ir a la página siguiente */}
         <button className={`btnSiguiente ${(isOpen) ? 'open' : 'closed'}`} onClick={nextPage} disabled={currentPage === totalPages}> Siguiente</button>
       </div>
