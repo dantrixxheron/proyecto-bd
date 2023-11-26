f2ec2747ff71b0b66b5ca44d61e321e8da7206e9
Daniela Cruz Mejía <123124430+dantrixxheron@users.noreply.github.com>
2023-11-26 01:40:10 +0000

    Refactor SidebarDBN component

--- src/components/SidebarDBN.tsx
+++ src/components/SidebarDBN.tsx
@@ -1,34 +1,28 @@
+
 import React, { useState, useEffect } from 'react';
 import SidebarContent from './SidebarContent';
 import { BiSolidCoinStack } from 'react-icons/bi';
 import { getDatabases } from '../lib/api/getDatabases';
-import { useAuth } from './AuthContext'; // Importa useAuth desde el archivo correcto
+import { useAuth } from './AuthContext'; // Asegúrate de importar useAuth desde el archivo correcto
 
-
-const SidebarDBN: React.FC = () => {
+const SidebarDBN: React.FC = ({}) => {
   const [databases, setDatabases] = useState<string[]>([]);
   const { user, password } = useAuth(); // Obtén el usuario y la contraseña desde el contexto
-
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        if (user && password) {
-          const databasesResult = await getDatabases(user, password); // Pasa el usuario y la contraseña
-          setDatabases(databasesResult || []); // Actualiza el estado con los resultados o un array vacío
-        }
-      } catch (error) {
-        console.error('Error al obtener las bases de datos:', error);
-      }
-    };
-
-    fetchData(); // Llama a la función fetchData dentro del useEffect
-  }, [user, password]); // Asegúrate de incluir user y password como dependencias del useEffect
+    if (user && password) {
+      const fetchData = async () => {
+        const databases = await getDatabases(user, password);
+        setDatabases(databases);
+      };
+      fetchData();
+    }
+  }, [user, password]);
 
   return (
     <div>
-      {databases.map((dbName) => (
-        <SidebarContent key={dbName} icon={BiSolidCoinStack} info={dbName} />
-      ))}
+        {databases.map((dbName) => (
+          <SidebarContent icon={BiSolidCoinStack} info={dbName} />
+        ))}
     </div>
   );
 };
